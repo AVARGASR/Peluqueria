92ce557a4b60eed98502466a937de0f68fe299d7
Ariel Vargas R <avargasutem@gmail.com>
2016-10-02 19:04:11 -0300

    Cambio en las vistas!

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,6 +3,6 @@ class Service < ActiveRecord::Base
   validates :tipo, :presence => {:message => "Campo nombre no debe estar en blanco"}, length: {in:3..40, too_short: "Demasiado corto", too_long: "Demasiado largo"}, uniqueness: {case_sensitive: false, message: "Ya esta registrado"}
   validates :precio, :numericality => { :greater_than_or_equal_to => 0, message: 'Formato de precio incorrecto, favor reintentar.' }
 
-  has_attached_file :cover, styles: {medium:"1280x720", thumb:"300x300", mini:"400x200"}
+  has_attached_file :cover, styles: {medium:"600x600", thumb:"300x300", mini:"200x200"}
   validates_attachment_content_type :cover, content_type: /\Aimage\/.*\Z/
 end
