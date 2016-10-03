c25b0e2ce78456bd0686491d711e20cf40cf49bc
Ariel Vargas R <avargasutem@gmail.com>
2016-10-02 21:33:55 -0300

    Cambios en fondos

--- db/schema.rb
+++ db/schema.rb
@@ -83,7 +83,6 @@ ActiveRecord::Schema.define(version: 20161002235334) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip",     limit: 255
     t.string   "last_sign_in_ip",        limit: 255
-    t.string   "username",               limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.integer  "permission_level",       limit: 4
@@ -93,4 +92,6 @@ ActiveRecord::Schema.define(version: 20161002235334) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "products", "categories"
+  add_foreign_key "services", "reservations"
 end
